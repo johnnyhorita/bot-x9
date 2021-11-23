f51934ad0228257910a2d90cdc72130f20bccc9b
luiztools <luizfduartejr@gmail.com>
2021-11-23 18:36:06 -0300

    AULA 02

--- index.js
+++ index.js
@@ -5,13 +5,18 @@
 const WebSocket = require('ws');
 const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1h');
 
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('<SEU TOKEN AQUI>');
+
 ws.onmessage = (event) => {
     const data = JSON.parse(event.data);
     const valor = parseFloat(data.k.c);
     console.log('Valor Atual:' + valor);
 
-    if (valor >= 4314)
+    if (valor >= 4200){
+        bot.telegram.sendMessage(<SEU CHAT ID AQUI>, 'Vender!');
         console.log('Vender!');
+    }
     else if (valor <= 4129)
         console.log('Comprar!');
 }
