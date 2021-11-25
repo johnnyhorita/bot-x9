2bc4f65b14ff3a12c1fe853d2ed620501235f80a
luiztools <luizfduartejr@gmail.com>
2021-11-25 02:01:52 -0300

    aula 3

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ Inscreva-se para dúvidas e suporte em https://www.luiztools.com.br/x9
 
 1. Abra o terminal e navegue com 'cd' até a pasta do projeto
 2. Instale as dependências com 'npm i'
-3. Execute o projeto com 'node index'
+3. Crie o seu bot de Telegram conforme ensinado em https://www.luiztools.com.br/post/como-enviar-mensagens-de-telegram-em-node-js-via-telegraf/
+4. Coloque seu token do bot de Telegram onde diz <SEU TOKEN AQUI>
+5. Coloque seu chat id do tutorial onde diz <CHAT ID AQUI>
+6. Execute o projeto com 'node index'
+
+Conheça meu curso completo em https://www.luiztools.com.br/curso-beholder
--- index.js
+++ index.js
@@ -2,23 +2,51 @@
 //max 4314
 //min 4129
 
-const WebSocket = require('ws');
-const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1h');
-
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf('<SEU TOKEN AQUI>');
-
-ws.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    const valor = parseFloat(data.k.c);
-    console.log('Valor Atual:' + valor);
-
-    if (valor >= 4200){
-        bot.telegram.sendMessage(<SEU CHAT ID AQUI>, 'Vender!');
-        console.log('Vender!');
+function calcRSI(prices) {
+    let ganhos = 0;
+    let perdas = 0;
+    for (let i = prices.length - 14; i < prices.length; i++) {
+        const diferenca = prices[i] - prices[i - 1];
+        if (diferenca >= 0)
+            ganhos += diferenca;
+        else
+            perdas -= diferenca;
     }
-    else if (valor <= 4129)
-        console.log('Comprar!');
+
+    const forcaRelativa = ganhos / perdas;
+    return 100 - (100 / (1 + forcaRelativa));
 }
 
-console.log('conectado');
+(async () => {
+
+    const axios = require('axios');
+    const candles = await axios.get('https://api.binance.com/api/v3/klines?symbol=ETHBUSD&interval=1m');
+    const closes = candles.data.map(candle => parseFloat(candle[4]));
+
+    const WebSocket = require('ws');
+    const ws = new WebSocket('wss://stream.binance.com:9443/ws/ethbusd@kline_1m');
+
+    const { Telegraf } = require('telegraf');
+    const bot = new Telegraf('<SEU TOKEN AQUI>');
+
+    ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.k.x) {
+            closes.push(parseFloat(data.k.c));
+            const rsi = calcRSI(closes);
+            console.log(rsi);
+
+            if (rsi > 70) {
+                bot.telegram.sendMessage(<CHAT ID AQUI>, 'Sobrecomprado!');
+                console.log('Sobrecomprado!');
+            }
+            else if (rsi < 30) {
+                bot.telegram.sendMessage(<CHAT ID AQUI>, 'Sobrevendido!');
+                console.log('Sobrevendido!');
+            }
+        }
+    }
+
+    console.log('conectado');
+
+})()
